8cdd0b5db92613d945cec766e454f17f5fb4f403
JesusLemos <jesuslemosfoncubierta@gmail.com>
2020-06-17 12:48:05 +0200

    Añadido mas productos y adaptacion del codigo

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,7 +7,8 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  menu = [{
+  menu = [
+  {
     CategoryId: 1,
     PlateName: "bravas",
     Price: 2.1,
@@ -16,12 +17,37 @@ export class Tab1Page {
     Image: "https://images.unsplash.com/photo-1565599837634-134bc3aadce8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
     Allergens: ["Wheat", "Peanut"],
     Amount:0
-  }]
+  },
+  {
+   CategoryId: 2,
+   PlateName: "Titaina",
+   Price: 3.1,
+   Addon: '',
+   Size: ["small", "medium", "large"],
+   Image: "https://images.unsplash.com/photo-1565599837634-134bc3aadce8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+   Allergens: ["Wheat", "Peanut"],
+   Amount:0
+},
+{
+  CategoryId: 3,
+  PlateName: "Esgarraet",
+  Price: 5.5,
+  Addon: '',
+  Size: ["small", "medium", "large"],
+  Image: "https://images.unsplash.com/photo-1565599837634-134bc3aadce8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  Allergens: ["Wheat", "Peanut"],
+  Amount:0
+},
+]
 
   price_Total= 0;
 
   constructor() { }
  prueba(){
-    this.price_Total = this.menu[0].Price * this.menu[0].Amount;
+   this.price_Total =0;
+   for(let i=0; i <= this.menu.length; i++){
+     
+     this.price_Total += this.menu[i].Price * this.menu[i].Amount;
+  }
   }
 }
